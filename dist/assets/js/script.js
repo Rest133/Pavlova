6c25d119c8513921581ac4c2b0cdd72bd3aeb834
Rest133 <31849309+Rest133@users.noreply.github.com>
2021-10-13 01:10:45 +0300

    To show

---
+++ dist/assets/js/script.js
@@ -0,0 +1,168 @@
+document.addEventListener('DOMContentLoaded', function () {
+    addBurgerMenuHandler()
+
+    popupHandler()
+
+    addFormsSendHandler()
+
+    addSameHeight()
+
+    addPhoneMasks()
+
+    openBenefitStructure()
+
+    addSlider()
+
+})
+
+function openBenefitStructure() {
+    let allElements = document.querySelectorAll('.benefit-structure__element')
+    allElements.forEach((currentElement) => {
+        currentElement.querySelector(".benefit-structure__element-row").addEventListener("click", evt => {
+            currentElement.querySelector('.benefit-structure__img').classList.toggle("active")
+            currentElement.querySelector(".benefit-structure__text").classList.toggle("active")
+        })
+    })
+}
+
+function addBurgerMenuHandler() {
+    let burgerMenu = document.querySelector('.burger-menu'),
+        menu = document.querySelector('.header-menu')
+    burgerMenu.addEventListener('click', () => {
+        burgerMenu.classList.toggle('active')
+        menu.classList.toggle('active')
+        if (menu.classList.contains('active')) {
+            document.body.style.overflow = 'hidden'
+        } else {
+            document.body.style.overflow = ''
+        }
+    })
+
+    menu.querySelectorAll('.header-element').forEach(menuItem => {
+        menuItem.addEventListener('click', () => {
+            if (menu.classList.contains('active')) {
+                burgerMenu.classList.remove('active')
+                menu.classList.remove('active')
+                document.body.style.overflow = ''
+            }
+        })
+    })
+}
+
+function popupHandler() {
+    let formBtns = document.querySelectorAll(".header-menu-btn"),
+        popup = document.querySelector(".wrapper__popup"),
+        closePopupBtn = popup.querySelector('.popup__close'),
+        allFormBtn = document.querySelectorAll('.complex-element__btn')
+
+    closePopupBtn.addEventListener('click', () => {
+        popup.classList.remove('active')
+        document.body.style.overflow = ''
+    })
+
+    formBtns.forEach(formBtn => {
+        formBtn.addEventListener("click", () => {
+            popup.classList.add("active")
+            document.body.style.overflow = 'hidden'
+        })
+    })
+
+    allFormBtn.forEach(formButton => {
+        formButton.addEventListener('click', () => {
+            popup.classList.add('active')
+            document.body.style.overflow = 'hidden'
+        })
+    })
+}
+
+function addFormsSendHandler() {
+    document.querySelectorAll('form').forEach(form => {
+        form.addEventListener('submit', event => {
+            formSend(event, form)
+        })
+    })
+}
+
+async function formSend(e, form) {
+    e.preventDefault()
+
+    let error = formValidate(form),
+        formData = new FormData(form)
+
+    if (error === 0) {
+        let response = await fetch('sendmail.php', {
+            method: 'POST',
+            body: formData
+        })
+        if (response.ok) {
+            let btnText = form.querySelector('button').textContent
+            form.querySelector('button').textContent = 'Отправлено'
+            setTimeout(function () {
+                form.reset()
+                form.querySelector('button').textContent = btnText
+            }, 1500)
+        } else {
+            form.querySelector('button').classList.add('error')
+            form.querySelector('button').textContent = 'Что-то пошло не так...'
+        }
+    } else {
+        console.log('error in form')
+    }
+
+}
+
+function formValidate(form) {
+    let error,
+        formInputs = form.querySelectorAll('input')
+
+    formInputs.forEach(formInput => {
+        if (formInput.required) {
+            error = 0
+        } else {
+            error = 1
+        }
+    })
+    return error
+}
+
+function addSameHeight() {
+    if (document.querySelector('.complexes-row') !== null) {
+        let allCards = document.querySelector('.complexes-row').querySelectorAll('.complex-element__text-part'),
+            maxHeight = 0
+
+        allCards.forEach(cardText => {
+            if (cardText.getBoundingClientRect().height > maxHeight) {
+                maxHeight = cardText.getBoundingClientRect().height
+            }
+        })
+
+        allCards.forEach(cardText => {
+            cardText.style.height = maxHeight + 'px'
+        })
+    }
+}
+
+function addPhoneMasks() {
+    let inputPhones = document.querySelectorAll('input[type=tel]');
+    let maskOptions = {
+        mask: '+{7}(000)000-00-00'
+    };
+    inputPhones.forEach(inputPhone => {
+        let mask = IMask(inputPhone, maskOptions);
+    })
+}
+
+function addSlider() {
+    $(document).ready(function () {
+        $(".slider").slick({
+            dots: true,
+            slidesToShow:3,
+            variableWidth:true,
+            adaptiveHeight:true,
+            centerMode:true
+
+
+
+        });
+    });
+}
